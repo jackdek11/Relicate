fdf8dd3d63706748b651d7bab1afd3880c7130e2
jack <jackdek11@gmail.com>
2023-07-16 13:29:00 +0200

    Added quickstart util scripts

---
+++ quickstart/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author Jack de Kock
+# Date: 16 July 2023
+
+docker compose down --remove-orphans --rmi local --volumes
+docker image prune
---
+++ quickstart/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author Jack de Kock
+# Date: 16 July 2023
+
+docker compose up --build
