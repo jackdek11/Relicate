2cf1da25ba12dc9ea50af8a29bc2982a7363265c
jack <jackdek11@gmail.com>
2023-07-16 17:51:04 +0200

    Added mock websocket connection route

--- repl/src/main.go
+++ repl/src/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
 )
 
-
 func main() {
-	// Create a new Fiber instance
 	app := fiber.New()
-  
-	// Define a route
-	app.Get("/", func(c *fiber.Ctx) error {
-	  return c.SendString("Hello, World!")
-	})
-  
+
+	// WebSocket route
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// Handle WebSocket connection
+		log.Println("New WebSocket connection")
+
+		// Read messages from the client
+		for {
+			// Read message from the client
+			msgType, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("Error reading message:", err)
+				break
+			}
+
+			// Print the received message
+			log.Printf("Received WebSocket message: %s", msg)
+
+			// Write message back to the client
+			err = c.WriteMessage(msgType, msg)
+			if err != nil {
+				log.Println("Error writing message:", err)
+				break
+			}
+		}
+
+		// Close the WebSocket connection
+		log.Println("Closing WebSocket connection")
+	}))
+
 	// Start the server
 	err := app.Listen(":3000")
 	if err != nil {
-	  panic(err)
+		log.Fatal(err)
 	}
-  }
+}
