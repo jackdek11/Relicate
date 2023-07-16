1cba06baf2279627c4779acccec5b5f17062e807
jack <jackdek11@gmail.com>
2023-07-16 21:12:28 +0200

    Added broadcastMessage function to echo websocket messages to everyone connected to the websocket channel

--- repl/src/main.go
+++ repl/src/main.go
@@ -10,8 +10,18 @@ import (
 func main() {
 	app := fiber.New()
 
+	// Create a WebSocketConnections instance to manage connections
+	connections := &WebSocketConnections{
+		connections: make([]*websocket.Conn, 0),
+	}
+
 	// WebSocket route
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// Add the new connection to the connections collection
+		connections.mu.Lock()
+		connections.connections = append(connections.connections, c)
+		connections.mu.Unlock()
+
 		// Handle WebSocket connection
 		log.Println("New WebSocket connection")
 
@@ -24,16 +34,19 @@ func main() {
 				break
 			}
 
-			// Print the received message
-			log.Printf("Received WebSocket message: %s", msg)
+			// Broadcast the message to all connected clients
+			broadcastMessage(connections, msgType, msg)
+		}
 
-			// Write message back to the client
-			err = c.WriteMessage(msgType, msg)
-			if err != nil {
-				log.Println("Error writing message:", err)
+		// Remove the connection from the connections collection
+		connections.mu.Lock()
+		for i, conn := range connections.connections {
+			if conn == c {
+				connections.connections = append(connections.connections[:i], connections.connections[i+1:]...)
 				break
 			}
 		}
+		connections.mu.Unlock()
 
 		// Close the WebSocket connection
 		log.Println("Closing WebSocket connection")
@@ -43,5 +56,19 @@ func main() {
 	err := app.Listen(":3000")
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+// Broadcasts a message to all connected clients
+func broadcastMessage(connections *WebSocketConnections, messageType int, message []byte) {
+	connections.mu.Lock()
+	defer connections.mu.Unlock()
+
+	for _, conn := range connections.connections {
+		err := conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Error writing message:", err)
+			continue
+		}
 	}
 }
