9bac37170769dc859ea35d8f90b04c65ccfc1996
Jack <jackdek11@gmail.com>
2023-08-30 20:52:09 +0200

    Used docker-compose commands v1 instead of v2

--- quickstart/clean.sh
+++ quickstart/clean.sh
@@ -2,5 +2,5 @@
 # Author Jack de Kock
 # Date: 16 July 2023
 
-docker compose down --remove-orphans --rmi local --volumes
+docker-compose down --remove-orphans --rmi local --volumes
 docker image prune
